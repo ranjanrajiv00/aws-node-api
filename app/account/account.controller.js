f3902e454740dda3e4ce8712d89bbf7c20d587d1
Rajiv Ranjan <rranjan@bngc02x6095jg5j.corp.local>
2018-12-04 16:21:48 +0530

    inital checkin

---
+++ app/account/account.controller.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+
+exports.authenticate = function (req, res) {
+    if (req.body.userName == 'microservice' && req.body.password == 'microservice') {
+        var token = jwt.sign(req.body.userName, 'microservice');
+
+        // return the information including token as JSON
+        res.json({
+            statusCode: 200,
+            message: 'Authenticated',
+            data: {
+                userName: req.body.userName,
+                token: token
+            }
+        });
+    }
+    else
+    {
+        res.json({ success: false, message: 'Authentication failed.' });
+    }
+};
